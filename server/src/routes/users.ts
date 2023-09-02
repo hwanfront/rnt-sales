df3c4ee4179241af8a577a1e7da8c8d03a30cdc9
hwanfront <kim015jh@gmail.com>
2023-09-02 22:41:24 +0900

    feat: 회원가입
    Resolves: #4

--- server/src/routes/users.ts
+++ server/src/routes/users.ts
@@ -1,10 +1,35 @@
 import * as express from 'express';
+import * as bcrypt from 'bcrypt';
 import passport from 'passport';
 
 import { checkAuthenticated } from './middleware';
 import User from '../models/user';
 
 const router = express.Router();
+
+router.post('/register', async (req, res, next) => {
+  try {
+    console.log(req.body.email);
+    const exUser = await User.findOne({
+      where: {
+        email: req.body.email,
+      },
+    });
+    console.log(exUser);
+    if(exUser) {
+      return res.status(403).send("이미 사용중인 이메일입니다.");
+    }
+    const hashedPassword = await bcrypt.hash(req.body.password, 12);
+    await User.create({
+      email: req.body.email,
+      nickname: req.body.nickname,
+      password: hashedPassword,
+    })
+    res.status(201).send("회원가입 성공!");
+  } catch (error) {
+    next(error);
+  }
+});
 
 router.post('/login', (req, res, next) => {
   passport.authenticate('local', (err: Error, user: User, info: { message: string }) => {
