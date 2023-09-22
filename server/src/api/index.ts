d2aef8e371dcd25af87b04ace2032875388f9fd1
hwanfront <kim015jh@gmail.com>
2023-09-23 01:49:47 +0900

    chore(BE): workspaceMember router 연결

--- server/src/api/index.ts
+++ server/src/api/index.ts
@@ -1,11 +1,13 @@
 import { Router } from 'express';
 import user from './routes/user';
 import workspace from './routes/workspace';
+import workspaceMember from './routes/workspaceMember';
 
 export default () => {
   const app = Router();
   user(app);
   workspace(app);
+  workspaceMember(app);
   
   return app;
 }
