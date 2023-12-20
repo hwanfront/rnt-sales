4db92426d9b0500f805e8bce940a1348bdf45cb5
hwanfront <65995108+hwanfront@users.noreply.github.com>
2023-12-20 21:31:52 +0900

    fix(BE): import 경로 수정 (#42)

--- server/src/models/item.ts
+++ server/src/models/item.ts
@@ -1,7 +1,7 @@
 import { DataTypes, Model } from 'sequelize';
 
-import { sequelize } from '@models';
 import type { SequelizeDB } from '@models';
+import { sequelize } from '@models/sequelize';
 import Workspace from '@models/workspace';
 
 import type { CreationOptional, ForeignKey, InferAttributes, InferCreationAttributes } from 'sequelize';
